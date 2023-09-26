f6a42c4bf102c89f2af0b8603dd9acf552dc89be
Roger David Sanchez Castro <roger.sanchez@tigo.com.hn>
2023-09-26 09:23:24 -0600

    primer commit

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+   @Output()
+   onDeletedCharacter: EventEmitter<string> = new EventEmitter();
+
+   @Input()
+   public characterList: Character[] = [{
+       name: 'Trunks',
+       power: 10
+   }]
+
+   emiteDeleteCharacter(id?: string): void{
+       if(!id) return;
+       this.onDeletedCharacter.emit(id);
+   }
+
+}
